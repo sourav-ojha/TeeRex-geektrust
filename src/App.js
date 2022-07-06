b7f4b7fb49ec7b58d0958c9111af00d218b46bf6
sourav-ojha <subhamojha9@gmail.com>
2022-07-06 20:59:35 +0530

    work starts now

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Home from "./pages/Home";
+import React from "react";
+import Cart from "./pages/Cart";
+import Header from "./components/Header";
 
 function App() {
+  const [cart, setCart] = React.useState([]);
+  const [mode, setmode] = React.useState(true);
+
+  const handleMode = () => {
+    setmode((prev) => !prev);
+  };
+
+  const cartOption = {
+    cart,
+    setCart,
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="">
+      <Header handleMode={handleMode} />
+
+      {mode ? <Home {...cartOption} /> : <Cart {...cartOption} />}
     </div>
   );
 }
